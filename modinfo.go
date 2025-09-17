cc86b3a4213a49f32ac82d3e2e6a13328b2a5a05
Alberto Massidda <amassidda@nvidia.com>
2025-09-17 11:48:43 +0200

    add tag to struct modinfo helps deserializing to a map
    Changelog: added

--- modinfo.go
+++ modinfo.go
@@ -2,10 +2,10 @@ package lsmod
 
 // ModInfo contains a module info provided by /proc/modules
 type ModInfo struct {
-	Depends   []string
-	Mem       uint64
+	Depends   []string `json:"depends"`
+	Mem       uint64   `json:"memory"`
 	Instances uint64
 	Offset    uint64
-	Taineds   []modTained
-	State     modState
+	Taineds   []modTained `json:"taineds"`
+	State     modState    `json:"state"`
 }
